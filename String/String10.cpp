acaee3622a17d25291e242894433912e87b2ce2e
Devika <devika999988@gmail.com>
2026-08-17 22:45:43 +0530

    Added a program to find the first non-repeating character

---
+++ String/String10.cpp
@@ -0,0 +1,25 @@
+//find the first non-repeating character
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    string s;
+
+    cout << "Enter a string: ";
+    cin >> s;
+
+    int freq[256] = {};
+
+    for (int i = 0; i < s.length(); i++) {
+        freq[s[i]]++;
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+        if (freq[s[i]] == 1) {
+            cout << " The first non repeating character: " << s[i];
+             break;
+        }
+    }
+    return 0;
+}
