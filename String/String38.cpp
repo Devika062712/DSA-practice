6fba23d67953aeccfffaf1c947b6ece7d25de4ec
Devika <devika999988@gmail.com>
2026-09-06 23:58:18 +0530

    Added a proghram to group anagrams together

--- String/String38.cpp
+++ String/String38.cpp
@@ -1,1 +1,30 @@
-//
+//To group anagrams together
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    vector<string> words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+    unordered_map<string, vector<string>> groups;
+
+    for (string word : words) {
+        string key = word;
+
+        sort(key.begin(), key.end());
+
+        groups[key].push_back(word);
+    }
+
+    for (auto group : groups) {
+        for (string word : group.second) {
+            cout << word << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
